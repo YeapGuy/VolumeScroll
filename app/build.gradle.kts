c4c139e669805e96c372e025d39eaeb65ba0f600
YeapGuy <yeapguy@gmail.com>
2026-03-29 14:44:50 +0200

    remove unneeded dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,8 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation("androidx.activity:activity:1.10.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
